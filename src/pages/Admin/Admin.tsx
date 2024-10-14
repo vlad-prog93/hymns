6e6783db73c8e5906aa48bb422a92ec572299436
Doroshenko Vladislav <vladka_@bk.ru>
2024-10-14 16:49:16 +0300

    начата разработка создания загрузки файла на сервер и обратно

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -5,8 +5,11 @@ import { toDeleteHymn } from '../../redux/reducers/ActionCreator'
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { ROUTES } from '../../utils/routes'
+import Button from '../../components/UI/Button/Button'
+import { useId } from 'react'
 
 const Admin = () => {
+  const id_input_file = useId()
   const navigate = useNavigate()
 
   const { hymns } = useAppSelector(state => state.hymnReducer)
@@ -22,11 +25,27 @@ const Admin = () => {
     navigate(`hymns/${id}`)
   }
 
+  const handleDownload = () => {
+    console.log('скачка началась')
+  }
+
   return (
     <div className={style.admin}>
 
       <h3 className={style.admin__title}>Выберите действие</h3>
-      <Link className={style.admin__link} to={ROUTES.hymns + ROUTES.newHymn} children='Создать гимн' />
+      <div className={style.admin__buttonContainer}>
+        <Link className={style.admin__link} to={ROUTES.hymns + ROUTES.newHymn} children='Создать гимн' />
+        <button className={style.admin__link} onClick={handleDownload} children='Скачать БД' />
+        <form className={style.admin__form}>
+          <label htmlFor={id_input_file}>Загрузить JSON
+
+            <input id='id_input_file' className={style.input__file} type='file' />
+
+          </label>
+          <button className={style.admin__link} onClick={handleDownload} children='Отправить' />
+        </form>
+      </div>
+
       <ul className={style.admin__list}>
         {hymns.map(hymn => {
           return (
